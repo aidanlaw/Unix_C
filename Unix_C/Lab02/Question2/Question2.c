194a6072ea41d2c8699739dfd12b6f5c688bec61
aidanlaw <law.aidan@gmail.com>
2015-08-11 22:37:34 +0800

    Second Push

---
+++ Unix_C/Lab02/Question2/Question2.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Question2
+//
+//  Created by Aidan Law on 11/08/2015.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

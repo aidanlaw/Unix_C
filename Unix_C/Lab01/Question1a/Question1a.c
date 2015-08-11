0429f6f33517bba45850ab7f6e93671fb134ccde
aidanlaw <law.aidan@gmail.com>
2015-08-11 22:58:03 +0800

    Question 1a Commit
    Commented code

--- Unix_C/Lab01/Question1a/Question1a.c
+++ Unix_C/Lab01/Question1a/Question1a.c
@@ -1,15 +1,24 @@
 //
-//  main.c
 //  Question1a
+//  Conditions
 //
 //  Created by Aidan Law on 11/08/2015.
 //
-//
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include <stdio.h>                  //required for input and output
+int main(void)
+{
+    int number;                     //set variable "number" as an integer
+    scanf("%d", &number);           //wait for user input and saves input as "number"
+    
+    if(number < 0 || number > 10)   //if number less than 0 or greater than 10...
+        printf("Out of range\n");   //output text and begin a new line
+    
+    else if(number != 5)            //if number is not equal to 5...
+        printf("Wrong\n");          //output text and begin a new line
+    
+    else                            //if previous statements are not fulfilled...
+        printf("Correct\n");        //output text and begin a new line
+    
+    return 0;                       //end program
 }

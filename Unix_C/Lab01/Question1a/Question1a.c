292933e4c81a8395474bb56e28ab3ddd82b5dc2c
aidanlaw <law.aidan@gmail.com>
2015-08-11 22:35:16 +0800

    Initial Push
    Initial push to Github servers

---
+++ Unix_C/Lab01/Question1a/Question1a.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Question1a
+//
+//  Created by Aidan Law on 11/08/2015.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
